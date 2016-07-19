d6d5a606c75943f6fca95bf047a9c9861d58a77d
akriti <akritikts@gmail.com>
2016-07-19 14:21:30 +0530

    Calculations added

---
+++ app/src/main/java/com/zappers/gola/Models/Calculations.java
@@ -0,0 +1,9 @@
+package com.zappers.gola.Models;
+
+/**
+ * Created by akriti on 19/7/16.
+ */
+public class Calculations {
+    public static int classes,present,absent;
+    public static double percent;
+}
